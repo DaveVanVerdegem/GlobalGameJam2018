f696bf7ebdcd48ca29285d3a3eeb4588fbd75e3d
Dave Van Verdegem <dave@pajamallama.be>
2018-01-27 00:07:58 +0100

    * Added hotspot code and prefab. * Added player prefab. * Added test scene and mockup wall & floor.

---
+++ Assets/Code/Agent.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class Agent : MonoBehaviour
+{
+	#region Inspector Fields
+	/// <summary>
+	/// Speed of this agent.
+	/// </summary>
+	[Tooltip("Speed of this agent.")]
+	[SerializeField]
+	private float _speed = 10f;
+	#endregion
+
+	#region Fields
+	///// <summary>
+	///// Rigid bosy component of this agent.
+	///// </summary>
+	//private Rigidbody2D _rigidBody;
+	#endregion
+
+	#region Life Cycle
+	// Use this for initialization
+	private void Awake()
+	{
+		// Get components.
+		//_rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		//Debug.Log(string.Format("This agent is on floor {0}", Level.Instance.ReturnFloorLevel(transform.position)));
+	}
+	#endregion
+
+	#region Movement
+	/// <summary>
+	/// Moves the agent horizontally with the given force.
+	/// </summary>
+	/// <param name="direction">Force to move agent. The sign will determine the direction.</param>
+	public void Move(float direction)
+	{
+		//_rigidBody.AddForce(Vector2.right * direction * _speed);
+		transform.Translate(Vector2.right * direction * _speed);
+	}
+	#endregion
+}
---
+++ Assets/Code/Level.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+	#region Inspector Fields
+	/// <summary>
+	/// Height of one floor level.
+	/// </summary>
+	[Tooltip("Height of one floor level.")]
+	[SerializeField]
+	private float _floorHeight = 1f;
+	#endregion
+
+	#region Static Properties
+	/// <summary>
+	/// Reference to the ingame level object.
+	/// </summary>
+	public static Level Instance;
+	#endregion
+
+	#region Life Cycle
+	// Use this for initialization
+	private void Awake()
+	{
+		if (Instance == null)
+			Instance = this;
+		else
+			Destroy(gameObject);
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	}
+	#endregion
+
+	#region Returns
+	/// <summary>
+	/// Returns back the floor level of the given position.
+	/// </summary>
+	/// <param name="position">Position to get floor level for.</param>
+	/// <returns>Returns the floor level as an int.</returns>
+	public int ReturnFloorLevel(Vector2 position)
+	{
+		return Mathf.FloorToInt(position.y / _floorHeight);
+	}
+	#endregion
+}
