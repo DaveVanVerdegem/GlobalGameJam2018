db50f082da55039f5fe64d69d39b81568d2f6b59
Dave Van Verdegem <dave@pajamallama.be>
2018-01-27 01:28:19 +0100

    * Made mockup level to test gameplay.

--- Assets/Code/AudioPlayer.cs
+++ Assets/Code/AudioPlayer.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 
 public class AudioPlayer : MonoBehaviour
 {
-    #region Singleton
+	#region Singleton
 
-    private static AudioPlayer _instance;
+	private static AudioPlayer _instance;
 
-    public static AudioPlayer Instance
-    {
-        get { return _instance ?? (_instance = FindObjectOfType<AudioPlayer>()); }
-        set { _instance = value; }
-    }
+	public static AudioPlayer Instance
+	{
+		get { return _instance ?? (_instance = FindObjectOfType<AudioPlayer>()); }
+		set { _instance = value; }
+	}
 
-    #endregion
-    #region Fields
+	#endregion
+	#region Fields
 
-    private AudioSource _audioSource;
-    #endregion
+	private AudioSource _audioSource;
+	#endregion
 
-    #region Life Cycle
+	#region Life Cycle
 
-    private void Awake()
-    {
-        _audioSource = GetComponent<AudioSource>();
-    }
+	private void Awake()
+	{
+		_audioSource = GetComponent<AudioSource>();
+	}
 
-    #endregion
+	#endregion
 
-    #region Methods
+	#region Methods
 
-    /// <summary>
-    /// Plays the given sound clip
-    /// </summary>
-    public void Play(AudioClip clip)
-    {
-        _audioSource.PlayOneShot(clip);
-    }
+	/// <summary>
+	/// Plays the given sound clip
+	/// </summary>
+	public void Play(AudioClip clip)
+	{
+		_audioSource.PlayOneShot(clip);
+	}
 
-    #endregion
+	#endregion
 }
