4fadc798d1e99b59b6a3cd08095243c548fe9be6
Julian Declercq <julian.declercq1@gmail.com>
2018-01-27 17:59:00 +0100

    Implemented basic UI. Implemented signal strenght icon. Added balancing to test easier.

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -5,227 +5,253 @@ using UnityEngine;
 [RequireComponent(typeof(Agent))]
 public class Player : MonoBehaviour
 {
-	#region Inspector Fields
-	/// <summary>
-	/// The material for the image to download.
-	/// </summary>
-	[Tooltip("The material for the image to download.")]
-	[SerializeField]
-	private Material _downloadMaterial = null;
-	#endregion
+    #region Inspector Fields
+    /// <summary>
+    /// The material for the image to download.
+    /// </summary>
+    [Tooltip("The material for the image to download.")]
+    [SerializeField]
+    private Material _downloadMaterial = null;
 
-	#region Static Properties
-	/// <summary>
-	/// Reference to this player object.
-	/// </summary>
-	public static Player Instance;
-	#endregion
+    /// <summary>
+    /// The icon that displays the signal strength.
+    /// </summary>
+    [SerializeField]
+    private SignalStrengthIcon _signalStrengthIcon = null;
 
-	#region Properties
-	/// <summary>
-	/// The floor the player is currently on.
-	/// </summary>
-	[HideInInspector]
-	public int Floor = 0;
+    #endregion
 
-	/// <summary>
-	/// Hideable object in range.
-	/// </summary>
-	[HideInInspector]
-	public HideableObject NearbyHideableObject;
+    #region Static Properties
+    /// <summary>
+    /// Reference to this player object.
+    /// </summary>
+    public static Player Instance;
+    #endregion
 
-	/// <summary>
-	/// Player is hidden from the enemy.
-	/// </summary>
-	[HideInInspector]
-	public bool Hidden = false;
-	#endregion
+    #region Properties
+    /// <summary>
+    /// The floor the player is currently on.
+    /// </summary>
+    [HideInInspector]
+    public int Floor = 0;
 
-	#region Fields
-	/// <summary>
-	/// Attached agent component.
-	/// </summary>
-	private Agent _agent;
+    /// <summary>
+    /// Hideable object in range.
+    /// </summary>
+    [HideInInspector]
+    public HideableObject NearbyHideableObject;
 
-	/// <summary>
-	/// Attached player renderer.
-	/// </summary>
-	private Renderer _playerRenderer = null;
+    /// <summary>
+    /// Player is hidden from the enemy.
+    /// </summary>
+    [HideInInspector]
+    public bool Hidden = false;
 
-	/// <summary>
-	/// Lists of the hotspots in range of this player.
-	/// </summary>
-	private List<Hotspot> _hotspotsInRange = new List<Hotspot>();
+    #endregion
 
-	/// <summary>
-	/// Hotspot that the player is currently connected to.
-	/// </summary>
-	private Hotspot _connectedHotspot;
-	/// <summary>
-	/// Is the player frozen aka prohibited to move?
-	/// </summary>
-	private bool _freeze = false;
+    #region Fields
+    /// <summary>
+    /// Attached agent component.
+    /// </summary>
+    private Agent _agent;
 
-	/// <summary>
-	/// Data that the player has already downloaded.
-	/// </summary>
-	private float _downloadedData;
+    /// <summary>
+    /// Attached player renderer.
+    /// </summary>
+    private Renderer _playerRenderer = null;
 
-	/// <summary>
-	/// The amount of total data to download.
-	/// </summary>
-	private float _dataToDownload = 100f;
-	#endregion
+    /// <summary>
+    /// Lists of the hotspots in range of this player.
+    /// </summary>
+    private List<Hotspot> _hotspotsInRange = new List<Hotspot>();
 
-	#region Life Cycle
-	// Use this for initialization
-	private void Awake()
-	{
-		// Set the instance.
-		if (Instance == null)
-			Instance = this;
+    /// <summary>
+    /// Hotspot that the player is currently connected to.
+    /// </summary>
+    private Hotspot _connectedHotspot;
+    /// <summary>
+    /// Is the player frozen aka prohibited to move?
+    /// </summary>
+    private bool _freeze = false;
 
-		// Get the needed components.
-		_agent = GetComponent<Agent>();
-		_playerRenderer = GetComponentInChildren<Renderer>();
-	}
+    /// <summary>
+    /// Data that the player has already downloaded.
+    /// </summary>
+    private float _downloadedData;
 
-	// Update is called once per frame
-	private void Update()
-	{
-		// Get all the hotspots in range of this player.
-		_hotspotsInRange = Hotspot.ReturnHotspotsInRange(this);
+    /// <summary>
+    /// The amount of total data to download.
+    /// </summary>
+    private float _dataToDownload = 100f;
+    #endregion
 
-		// Handle the inputs.
-		Inputs();
+    #region Life Cycle
+    // Use this for initialization
+    private void Awake()
+    {
+        // Set the instance.
+        if (Instance == null)
+            Instance = this;
 
-		if (_connectedHotspot != null)
-		{
-			// Disconnect if the hotspot is drained.
-			if (_connectedHotspot.Drained || _connectedHotspot.ReturnSignalStrength(this) == 0)
-				_connectedHotspot = null;
-			else
-			{
-				// Get data from the connected hot spot.
-				_downloadedData += _connectedHotspot.ReturnData(this);
+        // Get the needed components.
+        _agent = GetComponent<Agent>();
+        _playerRenderer = GetComponentInChildren<Renderer>();
+    }
 
-				// Visually update the progress
-				UpdateProgress();
-			}
-		}
-	}
+    // Update is called once per frame
+    private void Update()
+    {
+        // Get all the hotspots in range of this player.
+        _hotspotsInRange = Hotspot.ReturnHotspotsInRange(this);
 
-	private void OnDisable()
-	{
-		Instance = null;
-	}
+        // Handle the inputs.
+        Inputs();
 
-	private void OnApplicationQuit()
-	{
-		// Reset the progress material
-		UpdateProgress(true);
-	}
-	#endregion
+        if (_connectedHotspot == null)
+        {
+            // Show the no connection sprite
+            UpdateSignalStrengthIcon(0f);
+            return;
+        }
 
-	#region Methods
-	private void Inputs()
-	{
-		// Movement controls.
-		// Disable inputs when the player is frozen
-		if (_freeze)
-			return;
+        // Disconnect if the hotspot is drained.
+        float signalStrength = _connectedHotspot.ReturnSignalStrength(this);
+        if (_connectedHotspot.Drained || signalStrength < 0.001f)
+        {
+            // Disconnect the hotspot
+            _connectedHotspot = null;
 
-		// Movement controls.
-		if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
-		{
-			_agent.Move(Input.GetAxis("Horizontal") * Time.deltaTime);
-		}
+            // Show the no connection sprite
+            UpdateSignalStrengthIcon(0f);
+            return;
+        }
 
-		// Wifi controls.
-		if (Input.GetButtonUp("Connect"))
-		{
-			// Connect to new hotspot.
-			if (_connectedHotspot == null)
-			{
-				_connectedHotspot = ReturnBestAvailableHotspot();
+        // Get data from the connected hot spot.
+        _downloadedData += _connectedHotspot.ReturnData(this);
 
-				if (_connectedHotspot != null)
-					Debug.Log(string.Format("Connected to hot spot."));
-			}
-			// Disconnect from hotspot.
-			else
-			{
-				_connectedHotspot = null;
+        // Visually update the progress
+        UpdateProgress();
 
-				Debug.Log(string.Format("Disconnected from hotspot."));
-			}
-		}
+        // Update the sprite
+        UpdateSignalStrengthIcon(signalStrength);
+    }
 
-		// Hideable controls.
-		if (Input.GetButtonDown("Activate") && NearbyHideableObject != null)
-			Hide(!Hidden);
-	}
-	/// <summary>
-	/// Disable the players input or not.
-	/// </summary>
-	/// <param name="freeze">Freeze the inputs of the player.</param>
-	public void Freeze(bool freeze = true)
-	{
-		_freeze = freeze;
-	}
+    private void OnDisable()
+    {
+        Instance = null;
+    }
 
-	/// <summary>
-	/// Visually update the progress of downloading the graphic.
-	/// </summary>
-	/// <param name="reset"></param>
-	private void UpdateProgress(bool reset = false)
-	{
-		float progress = (reset) ? 0f : _downloadedData / _dataToDownload;
-		_downloadMaterial.SetFloat("_Progress", progress);
-	}
+    private void OnApplicationQuit()
+    {
+        // Reset the progress material
+        UpdateProgress(true);
+    }
+    #endregion
 
-	public void Hide(bool hide)
-	{
-		// Update the state.
-		Hidden = hide;
+    #region Methods
+    private void Inputs()
+    {
+        // Movement controls.
+        // Disable inputs when the player is frozen
+        if (_freeze)
+            return;
 
-		// Make the player invisible.
-		_playerRenderer.enabled = !hide;
-	}
-	#endregion
+        // Movement controls.
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
+        {
+            _agent.Move(Input.GetAxis("Horizontal") * Time.deltaTime);
+        }
 
-	#region Returns
-	/// <summary>
-	/// Get the strongest hotspot in range of this player.
-	/// </summary>
-	/// <returns>Returns a hotspot.</returns>
-	private Hotspot ReturnBestAvailableHotspot()
-	{
-		// No hotspots in range.
-		if (_hotspotsInRange.Count == 0)
-			return null;
+        // Wifi controls.
+        if (Input.GetButtonUp("Connect"))
+        {
+            // Connect to new hotspot.
+            if (_connectedHotspot == null)
+            {
+                _connectedHotspot = ReturnBestAvailableHotspot();
 
-		// Get strongest hot spot in range.
-		Hotspot strongestHotspot = null;
+                if (_connectedHotspot != null)
+                    Debug.Log(string.Format("Connected to hot spot."));
+            }
+            // Disconnect from hotspot.
+            else
+            {
+                _connectedHotspot = null;
 
-		for (int i = 0; i < _hotspotsInRange.Count; i++)
-		{
-			// Skip drained hotspots.
-			if (_hotspotsInRange[i].Drained)
-				continue;
+                Debug.Log(string.Format("Disconnected from hotspot."));
+            }
+        }
 
-			if (strongestHotspot == null)
-			{
-				strongestHotspot = _hotspotsInRange[i];
-			}
-			else
-			{
-				if (_hotspotsInRange[i].ReturnSignalStrength(this) > strongestHotspot.ReturnSignalStrength(this))
-					strongestHotspot = _hotspotsInRange[i];
-			}
-		}
+        // Hideable controls.
+        if (Input.GetButtonDown("Activate") && NearbyHideableObject != null)
+            Hide(!Hidden);
+    }
+    /// <summary>
+    /// Disable the players input or not.
+    /// </summary>
+    /// <param name="freeze">Freeze the inputs of the player.</param>
+    public void Freeze(bool freeze = true)
+    {
+        _freeze = freeze;
+    }
 
-		return strongestHotspot;
-	}
-	#endregion
+    /// <summary>
+    /// Visually update the progress of downloading the graphic.
+    /// </summary>
+    /// <param name="reset"></param>
+    private void UpdateProgress(bool reset = false)
+    {
+        float progress = (reset) ? 0f : _downloadedData / _dataToDownload;
+        _downloadMaterial.SetFloat("_Progress", progress);
+    }
+
+    public void Hide(bool hide)
+    {
+        // Update the state.
+        Hidden = hide;
+
+        // Make the player invisible.
+        _playerRenderer.enabled = !hide;
+    }
+
+    public void UpdateSignalStrengthIcon(float signalStrength)
+    {
+        _signalStrengthIcon.UpdateSprite(signalStrength);
+    }
+    #endregion
+
+    #region Returns
+    /// <summary>
+    /// Get the strongest hotspot in range of this player.
+    /// </summary>
+    /// <returns>Returns a hotspot.</returns>
+    private Hotspot ReturnBestAvailableHotspot()
+    {
+        // No hotspots in range.
+        if (_hotspotsInRange.Count == 0)
+            return null;
+
+        // Get strongest hot spot in range.
+        Hotspot strongestHotspot = null;
+
+        for (int i = 0; i < _hotspotsInRange.Count; i++)
+        {
+            // Skip drained hotspots.
+            if (_hotspotsInRange[i].Drained)
+                continue;
+
+            if (strongestHotspot == null)
+            {
+                strongestHotspot = _hotspotsInRange[i];
+            }
+            else
+            {
+                if (_hotspotsInRange[i].ReturnSignalStrength(this) > strongestHotspot.ReturnSignalStrength(this))
+                    strongestHotspot = _hotspotsInRange[i];
+            }
+        }
+
+        return strongestHotspot;
+    }
+    #endregion
 }
