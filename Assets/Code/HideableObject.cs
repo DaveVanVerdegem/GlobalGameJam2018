9b25bd5059e650a40c08b036db8bd1644d927916
Dave Van Verdegem <dave@pajamallama.be>
2018-01-28 02:46:58 +0100

    * Added curtain as hideable object.

--- Assets/Code/HideableObject.cs
+++ Assets/Code/HideableObject.cs
@@ -4,39 +4,65 @@ using UnityEngine;
 
 public class HideableObject : MonoBehaviour
 {
-    #region Fields
+	#region Inspector Fields
+	/// <summary>
+	/// Visuals for the open state of the object.
+	/// </summary>
+	[Tooltip("Visuals for the open state of the object.")]
+	[SerializeField]
+	private GameObject _openVisuals;
 
-    #endregion
+	/// <summary>
+	/// Visuals for the closed state of the object.
+	/// </summary>
+	[Tooltip("Visuals for the closed state of the object.")]
+	[SerializeField]
+	private GameObject _closedVisuals;
+	#endregion
 
-    #region Life Cycle
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        // Update the player reference
-        if (other.CompareTag("Player"))
-        {
-            Player.Instance.NearbyHideableObject = this;
-            Player.Instance.ShowInteractableIcon();
-        }
-    }
+	#region Fields
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        // Ignore everything but the player
-        if (!other.CompareTag("Player"))
-            return;
+	#endregion
 
-        // Make sure the player is not hidden anymore
-        Player.Instance.Hide(false);
+	#region Life Cycle
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		// Update the player reference
+		if (other.CompareTag("Player"))
+		{
+			Player.Instance.NearbyHideableObject = this;
+			Player.Instance.ShowInteractableIcon();
+		}
+	}
 
-        // Clear the reference
-        Player.Instance.NearbyHideableObject = null;
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		// Ignore everything but the player
+		if (!other.CompareTag("Player"))
+			return;
 
-        // Hide interactable icon
-        Player.Instance.ShowInteractableIcon(false);
-    }
+		// Make sure the player is not hidden anymore
+		Player.Instance.Hide(false);
+		ToggleState(true);
 
-    #endregion
+		// Clear the reference
+		Player.Instance.NearbyHideableObject = null;
 
-    #region Methods
-    #endregion
+		// Hide interactable icon
+		Player.Instance.ShowInteractableIcon(false);
+	}
+
+	#endregion
+
+	#region Methods
+	/// <summary>
+	/// Toggle the closed state of this objects visuals.
+	/// </summary>
+	/// <param name="open">Set this object to open.</param>
+	public void ToggleState(bool open)
+	{
+		_openVisuals.SetActive(open);
+		_closedVisuals.SetActive(!open);
+	}
+	#endregion
 }
